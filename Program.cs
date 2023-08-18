4a1cd0d8460fdf6107df25e059b9e6dc4dd9a537
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-18 11:18:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TervehdysNimellä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kysytään käyttäjältä nimi ja tallennetaan se muuttujaan
+            Console.WriteLine("Syötä nimesi:");
+            string nimi = Console.ReadLine();
+
+            // Tulostetaan tervehdys käyttäjän nimellä
+            Console.WriteLine("Hei " + nimi);
+
+            // Pysäytetään ohjelma odottamaan syötettä ennen sulkemista
+            Console.ReadLine();
+        }
+    }
+}
